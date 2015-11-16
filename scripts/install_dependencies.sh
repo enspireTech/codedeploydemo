a93e2d02aec2e5ab6f10220dc1225bfae89b2980
Andrew Page <andrew.page32@gmail.com>
2015-11-16 11:58:31 -0500

    Add conditional statement for maven dependency

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -23,10 +23,17 @@ nvm install v0.8.12
 
 # Maven
 cd /usr/local/src
-wget http://archive.apache.org/dist/maven/binaries/apache-maven-3.0.4-bin.tar.gz
-tar -zxf apache-maven-3.0.4-bin.tar.gz
-sudo cp -R apache-maven-3.0.4 /usr/local
-sudo ln -s /usr/local/apache-maven-3.0.4/bin/mvn /usr/bin/mvn
+
+file="/usr/bin/mvn"
+if[ -f "$file"]
+then
+    echo "$file found; skipping step"
+else
+    wget http://archive.apache.org/dist/maven/binaries/apache-maven-3.0.4-bin.tar.gz
+    tar -zxf apache-maven-3.0.4-bin.tar.gz
+    sudo cp -R apache-maven-3.0.4 /usr/local
+    sudo ln -s /usr/local/apache-maven-3.0.4/bin/mvn /usr/bin/mvn
+fi
 
 
 
